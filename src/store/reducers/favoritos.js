a2123500bb7e3c52d219f9da73b901f9edb4aa91
matias robles <matias.robles1601@gmail.com>
2020-12-09 16:44:55 -0300

    se me pasó

---
+++ src/store/reducers/favoritos.js
@@ -0,0 +1,34 @@
+import { horariosConstants } from '../constants/horarios'
+
+const initialState = {
+    data: []
+}
+
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case horariosConstants.FAV:
+            recorridos = [...state.data]
+            let reco = recorridos.map(x => {
+                console.log('X: ', x)
+                if (x.origen == action.horario.origen && x.destino == action.horario.destino) {
+                    x.Horarios.push(action.horario)
+                }
+                return x
+            })
+            return ({
+                ...state,
+                data: reco
+            })
+            break
+        case horariosConstants.LOAD:
+            return ({
+                ...state,
+                data: action.favoritos
+            })
+            break
+        default:
+            return { ...state }
+
+    }
+}
